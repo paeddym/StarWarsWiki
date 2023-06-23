733a532c8b4c1404f1729bc262c6df647fc681fa
paeddym <patrikmarosvary@gmail.com>
2023-06-23 23:27:50 +0200

    Erster Draft People

--- src/components/People.vue
+++ src/components/People.vue
@@ -1,11 +1,34 @@
-<Script>
-
-</Script>
-
 <template>
-
-</template>
-
-<style scoped>
-
-</style>
+    <div>
+      <h1>Star Wars Charaktere</h1>
+      <ul>
+        <li v-for="character in characters" :key="character.name">
+          {{ character.name }}
+        </li>
+      </ul>
+      <button @click="fetchCharacters">Charaktere aktualisieren</button>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        characters: []
+      };
+    },
+    methods: {
+      fetchCharacters() {
+        axios.get('https://swapi.dev/api/people/')
+          .then(response => {
+            this.characters = response.data.results;
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    }
+  };
+  </script>
