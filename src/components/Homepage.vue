c55d531c8f5b1c925b780be14befd23dfbc412a7
paeddym <patrikmarosvary@gmail.com>
2023-06-24 11:01:47 +0200

    Added Homepage

---
+++ src/components/Homepage.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="star-wars-theme">
+      <h1 class="title">Willkommen zur Star Wars Homepage</h1>
+      <div class="content">
+        <p class="intro">Möge die Macht mit dir sein!</p>
+        <img class="logo" src="./assets/star-wars-logo.png" alt="Star Wars Logo">
+        <router-link class="button" to="/People">Discover People</router-link>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    // Komponentenoptionen
+  };
+  </script>
+  
+  <style scoped>
+  .star-wars-theme {
+    background-color: black;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    font-family: 'Arial', sans-serif;
+  }
+  
+  .title {
+    font-size: 3rem;
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+  
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .intro {
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+    text-align: center;
+  }
+  
+  .logo {
+    width: 200px;
+    height: auto;
+    margin-bottom: 2rem;
+  }
+  
+  .button {
+    padding: 1rem 2rem;
+    background-color: #ffe81f;
+    color: black;
+    font-size: 1.2rem;
+    text-decoration: none;
+    border-radius: 0.5rem;
+    transition: background-color 0.3s ease;
+  }
+  
+  .button:hover {
+    background-color: #ffd700;
+  }
+  </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
+    {                                                                 //Homepage
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Home',
+      component: () => import('../components/Homepage.vue')
     },
     {
       path: '/People',
@@ -33,14 +32,6 @@ const router = createRouter({
       path: '/Species',
       name: 'Species',
       component: () => import('../components/Species.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
     }
   ]
 })
